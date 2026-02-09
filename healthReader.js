8d3fa92b00811fd6d094633224671de20e16fb5b
Ekko Nguyen <nguyen.ekko@ic.edu>
2026-02-08 22:26:03 -0600

    health reader implemented

---
+++ healthReader.js
@@ -0,0 +1,18 @@
+const fs = require('fs/promises');
+
+async function readHealth() {
+  try {
+    const data = await fs.readFile('data/health-metrics.json', 'utf8');
+    const healthData = JSON.parse(data);
+
+    const totalEntries = healthData.metrics.length;
+
+    console.log("Success! Found your health data!");
+    console.log("Total health entries:", totalEntries);
+
+  } catch (error) {
+    console.log("Something went wrong:", error.message);
+  }
+}
+
+readHealth();
